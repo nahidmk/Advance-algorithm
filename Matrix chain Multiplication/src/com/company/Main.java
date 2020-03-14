26090be90d167700706e767be7df1dd4d9eff2e4
Nahid <empiremanik@gmail.com>
2020-03-14 19:18:37 +0600

    14/03/20

--- Matrix chain Multiplication/src/com/company/Main.java
+++ Matrix chain Multiplication/src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,15 +15,14 @@ public class Main {
 
     private static int mcm(int[] arr, int n) {
         int t[][] = new int[n][n];
+        int s[][] = new int[n][n];
+        List<String> matrix = new ArrayList<>();
+        matrix.add("(");
         for(int l = 2;l<n;l++)
         {
             for(int i = 1;i<n-l+1;i++)
             {
                 int j = i+l-1;
-                if(j==i)
-                {
-                    t[i][j]=0;
-                }
                 t[i][j] = Integer.MAX_VALUE;
                 for(int k = i;k<j;k++)
                 {
@@ -27,11 +30,41 @@ public class Main {
                     if(t[i][j]>value)
                     {
                         t[i][j] = value;
+                        s[i][j] = k;
                     }
                 }
             }
         }
-
+//        for(int i = 1;i<n-1;i++)
+//        {
+//            for(int j =2;j<n;j++)
+//            {
+//                System.out.print(s[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+        for(int i = 0;i<n-1;i++)
+        {
+            matrix.add("M"+(i+1));
+        }
+        matrix.add(")");
+        calculateExpresion(s,1,n-1);
+        System.out.println();
         return t[1][n-1];
+    }
+
+    private static void calculateExpresion(int[][] s, int i,int j) {
+        if(i==j)
+        {
+            System.out.print("M"+i+" ");
+        }else
+        {
+            System.out.print("(");
+            calculateExpresion(s,i,s[i][j]);
+            calculateExpresion(s,s[i][j]+1,j);
+            System.out.print(")");
+        }
+
+
     }
 }
